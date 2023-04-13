87a56b9c1736b53f4d3e15a1118319f313c27e3e
JPaulo <gama-leao@hotmail.com>
2023-04-13 19:14:20 +0100

    working database

--- src/main/java/org/academiadecodigo/bootcamp/glass/LoginController.java
+++ src/main/java/org/academiadecodigo/bootcamp/glass/LoginController.java
@@ -1,5 +1,13 @@
 package org.academiadecodigo.bootcamp.glass;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/frozenShop")
 public class LoginController {
+
+
+
 
 }
--- src/main/java/org/academiadecodigo/bootcamp/glass/model/products/LightSub.java
+++ src/main/java/org/academiadecodigo/bootcamp/glass/model/products/LightSub.java
@@ -1,5 +1,8 @@
 package org.academiadecodigo.bootcamp.glass.model.products;
 
+import javax.persistence.Entity;
+
+@Entity
 public class LightSub extends Products {
 
 }
