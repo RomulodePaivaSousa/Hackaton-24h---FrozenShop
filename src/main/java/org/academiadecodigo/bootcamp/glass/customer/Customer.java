6f55cdeda6dbb965dddcfa77d8d5373dc29d3486
JPaulo <gama-leao@hotmail.com>
2023-04-13 17:13:15 +0100

    minor updates

--- src/main/java/org/academiadecodigo/bootcamp/glass/customer/Customer.java
+++ src/main/java/org/academiadecodigo/bootcamp/glass/customer/Customer.java
@@ -1,4 +1,22 @@
 package org.academiadecodigo.bootcamp.glass.customer;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer extends AbstractModel{
+
+    @Column
+    private String name;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String address;
+    @Column(unique = true)
+    private int phone;
+
+
+
 }
